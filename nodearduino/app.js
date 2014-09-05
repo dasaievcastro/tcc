59d5766f3b5943470a58aaba0ccaeb9d0745f4a3
Dasaiev Castro <dasaievcastroster@gmail.com>
2014-09-05 12:55:12 -0300

    removendo arquivos não usados na pasta nodearduino

--- nodearduino/app.js
+++ nodearduino/app.js
@@ -1,30 +1,30 @@
 var express = require('express.io')
-,	app = express()
-,	passport = require('passport')
-, 	ejs = require('ejs')
-, 	flash = require('connect-flash')
-,	mongoClient = require('mongodb').MongoClient
-,	rotas = require('./rotas')
-,	localStrategy = require('passport-local').Strategy
-,	autenticacao = require('./autenticacao');
+    , app = express()
+    , passport = require('passport')
+    , ejs = require('ejs')
+    , flash = require('connect-flash')
+    , mongoClient = require('mongodb').MongoClient
+    , rotas = require('./rotas')
+    , localStrategy = require('passport-local').Strategy
+    , autenticacao = require('./autenticacao');
 
 app.http().io();
 app.configure(function() {
-	app.use(express.compress());
+    app.use(express.compress());
     app.set('view engine', 'ejs');
-	app.use(express.static(__dirname + '/public'));
-	app.set('views', __dirname + '/views');
-	app.use(express.cookieParser());
-	app.use(express.bodyParser());
-	app.use(express.session({ secret: 'hashdesegurancaprojetoarduino' }));
-	app.use(flash());
+    app.use(express.static(__dirname + '/public'));
+    app.set('views', __dirname + '/views');
+    app.use(express.cookieParser());
+    app.use(express.bodyParser());
+    app.use(express.session({secret: 'hashdesegurancaprojetoarduino'}));
+    app.use(flash());
     app.use(passport.initialize());
     app.use(passport.session());
 });
 
 mongoClient.connect('mongodb://127.0.0.1:27017/aplicacao', function(err, db) {
-autenticacao(passport,localStrategy,db);
-rotas(app,db,passport);
+    autenticacao(passport, localStrategy, db);
+    rotas(app, db, passport);
 
 });
 
