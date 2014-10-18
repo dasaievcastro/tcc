73c8c4835244ebfa192e2fec28df3a9831bdcd7c
dasaievcastro <dasaievcastroster@gmail.com>
2014-10-18 14:00:03 -0300

    Versão beta da apresentação final

--- Arduíno/hello2_semComentarios/hello2_semComentarios.ino
+++ Arduíno/hello2_semComentarios/hello2_semComentarios.ino
@@ -15,7 +15,7 @@ SocketIOClient client;
 byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEE};
 
 /*IP de configuração do servidor*/
-char hostname[] = "192.168.1.4"; //"54.207.65.161"; //"192.168.0.25";// //
+char hostname[] = "192.168.1.2"; //"54.207.65.161"; //"192.168.0.25";// //
 
 /*configurar porta do socket.io*/
 const int port = 8087;
@@ -41,11 +41,15 @@ int sensorLuz = A4;
 int interruptor = 18;
 int ledCopa = 22;
 int ledLuzes = 34;
+int ledAlarme = 36;
 int ventilador = 24;
+int ultrasonicoEnvia = 38, ultrasonicoRecebe = 40, Pinofalante = 4, tempo=5, pararAlarme=1;
+
 /*Objeto para representar thread de leitura da temperatura do comodo da cada*/
 Thread threadTemperatura;
 Thread threadLuminosidade;
-
+Thread threadAlarme;
+Thread threadTocarAlarme;
 
 Opcao opcoes[4] = {
   {"salaOFF", "salaDesligado", "desliga", 40, 0},
@@ -106,10 +110,11 @@ void ondata(SocketIOClient client, char *data) {
     client.send("luzesLigado"); // envia pra todos clientes
     Serial.println("liga");
     digitalWrite(ledLuzes, HIGH); ////////////////////////////////////fim cozinha
-  } else if (str == "quartoOFF") {// recebe de
-    client.send("quartoDesligado"); // envia pra todos clientes
+  } if (str == "alarmeOFF") {// recebe de
+    client.send("alarmeDesligado"); // envia pra todos clientes
     Serial.println("desliga");
-    digitalWrite(3, LOW);
+    noTone(Pinofalante);
+    pararAlarme=1;
   } else if (str == "quartoON") {// recebe de
     client.send("quartoLigado"); // envia pra todos clientes
     Serial.println("liga");
@@ -138,15 +143,7 @@ void ondata(SocketIOClient client, char *data) {
     client.send("portaoLigado"); // envia pra todos clientes
     Serial.println("liga");
     digitalWrite(46, HIGH); ////////////////////////////////////fim portao
-  } else if (str == "alarmeOFF") {// recebe de
-    client.send("alarmeDesligado"); // envia pra todos clientes
-    Serial.println("desliga");
-    digitalWrite(47, LOW);
-  } else if (str == "alarmeON") {// recebe de
-    client.send("alarmeLigado"); // envia pra todos clientes
-    Serial.println("liga");
-    digitalWrite(47, HIGH);
-  } ////////////////////////////////////fim alarme
+  }  ////////////////////////////////////fim alarme
   //    } else if (str == "novacor255,255,255") {
   //        //esquema do led
   //        analogWrite(greenpin, 255);
@@ -168,8 +165,8 @@ void medeTemperatura() {
   }
   temp = temp/100;
   temp = (temp * 0.488);
-  Serial.print("temperatura:");
-  Serial.println(temp);
+  //Serial.print("temperatura:");
+  temp = temp -1;
   if (temp > 33) {
     ligaVentilador(String(temp));
   } else {
@@ -183,13 +180,46 @@ void medeTemperatura() {
 void medeLuminosidade() {
   
   int luminosidade = analogRead(sensorLuz);
-  Serial.println(luminosidade);
+  //Serial.println(luminosidade);
   if (luminosidade < 300) {
     ligaLampadas();
   } else {
     desligaLampadas();
   }
   delay(20);
+}
+
+void medeAlarme(){
+ digitalWrite(ultrasonicoEnvia, LOW); 
+ delayMicroseconds(2); 
+
+ digitalWrite(ultrasonicoEnvia, HIGH);
+ delayMicroseconds(10); 
+ 
+ digitalWrite(ultrasonicoEnvia, LOW);
+ long duration = pulseIn(ultrasonicoRecebe, HIGH);
+ 
+ //Calculate the distance (in cm) based on the speed of sound.
+ long distance = duration/58.2; 
+ if(distance<11){
+     pararAlarme=0;
+ }
+}
+
+void tocarAlarme(){
+  if(pararAlarme==0){
+    client.send("alarmeLigado");
+    for (int frequencia = 150; frequencia < 1800; frequencia += 1) 
+    {
+      tone(Pinofalante, frequencia, tempo); 
+      //delay(1);
+    }
+    for (int frequencia = 1800; frequencia > 150; frequencia -= 1) 
+    {
+      tone(Pinofalante, frequencia, tempo); 
+      //delay(1);
+    }
+  }
 }
 
 void ligaVentilador(String temp) {
@@ -235,8 +265,12 @@ void setup() {
   pinMode(ventilador, OUTPUT);
   //pin lampadas
   pinMode(ledLuzes, OUTPUT);
+  //luz alarme
+  pinMode(ultrasonicoEnvia, OUTPUT);
+  pinMode(ultrasonicoRecebe, INPUT);
+  pinMode(Pinofalante,OUTPUT);
+  
   //pinos rgb
-
   pinMode(redpin, OUTPUT);
   pinMode(bluepin, OUTPUT);
   pinMode(greenpin, OUTPUT);
@@ -255,15 +289,19 @@ void setup() {
   /*Setando thread pra verificar temperaturas*/
   threadTemperatura.setInterval(1000);
   threadTemperatura.onRun(medeTemperatura);
-  threadLuminosidade.setInterval(2020);
+  threadLuminosidade.setInterval(1000);
   threadLuminosidade.onRun(medeLuminosidade);
+  threadAlarme.setInterval(200);
+  threadAlarme.onRun(medeAlarme);
+  threadTocarAlarme.setInterval(4000);
+  threadTocarAlarme.onRun(tocarAlarme);
   // quarto com arduino UNO
   pinMode(3, OUTPUT);
 
   //interruptor
 
   attachInterrupt(5, mudar, HIGH);
-
+  attachInterrupt(4, alarme, CHANGE );
   /*Iniciando conexão com internet*/
   Ethernet.begin(mac);
   delay(1000);
@@ -288,6 +326,12 @@ void loop() {
   if (threadLuminosidade.shouldRun()) {
     threadLuminosidade.run();
   }
+  if (threadAlarme.shouldRun()) {
+    threadAlarme.run();
+  }
+  if (threadTocarAlarme.shouldRun()) {
+    threadTocarAlarme.run();
+  }
 
   //instanciando leitor do socket.io
   client.monitor();
@@ -309,4 +353,10 @@ void mudar() {
       client.send("copaLigado");
     }
   }
+}
+
+void alarme(){
+  Serial.println("para de invadir");
+  client.send("alarmeLigado");
+  digitalWrite(ledAlarme,HIGH);
 }
